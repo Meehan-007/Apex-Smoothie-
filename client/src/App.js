b4def4e9337e33847acb8cec7128e6a4cb627e08
Meehan-007 <93944358+Meehan-007@users.noreply.github.com>
2022-09-01 13:36:47 -0400

    front end setup

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg'; 
+import './index.css' 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return ( 
+    <Router> 
+    <div className="App">
+      
+    </div> 
+    </Router>
+  );
+}
+
+export default App;
