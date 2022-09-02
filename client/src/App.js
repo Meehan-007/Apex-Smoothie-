168a7664ffdd4bdd95d74d74a7c7a5d71532ad2f
Meehan-007 <93944358+Meehan-007@users.noreply.github.com>
2022-09-01 21:36:50 -0400

    CSS and assest uploaded

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,35 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return ( 
     <Router> 
-    <div className="App">
-      
+    <div className="App"> 
+    <div className='container'> 
+    <Routes> 
+      <Route 
+        path="/" 
+        element={<Home />} 
+      />
+      <Route 
+        path="/login" 
+        element={<Login />} 
+      />
+      <Route 
+        path="/signup" 
+        element={<Signup />} 
+      />
+      <Route 
+        path="/profile" 
+        element={<Profile />} 
+      />
+      <Route 
+        path="/thought/:id" 
+        element={<SingleThought />} 
+      />
+      <Route 
+        path="*" 
+        element={<NoMatch />} 
+      />
+    </Routes>
+    </div>  
     </div> 
     </Router>
   );
