46d0a06f1cb722274142bba237bd69a9103ae7ea
Meehan-007 <93944358+Meehan-007@users.noreply.github.com>
2023-03-01 15:36:00 -0500

    package.json being updated

--- backend/config/connection.js
+++ backend/config/connection.js
@@ -1,5 +1,5 @@
-require('dotenv').config();
 
+require('dotenv').config
 
 const Sequelize = require('sequelize');
 
@@ -8,10 +8,14 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize('smoothie_db', 'root', 'Root122!', {
+   sequelize = new Sequelize('smoothie_db', 'root', 'Root122!', {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
   });
+
+
+
+
 }
 module.exports = sequelize;
