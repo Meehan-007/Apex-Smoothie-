f4e19898d1beadfa3b9f7e2943680e564dac21e3
Meehan-007 <93944358+Meehan-007@users.noreply.github.com>
2024-07-09 20:17:41 -0400

    Update index.js casing

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const smoothieRoutes = require('./Home-Routes');
-const menuRoutes = require('./Menu-Routes');
+const smoothieRoutes = require('./home-Routes');
+const menuRoutes = require('./menu-Routes');
 
 // add prefix of `/pizzas` to routes created in `pizza-routes.js`
 router.use('/smoothie', smoothieRoutes);
