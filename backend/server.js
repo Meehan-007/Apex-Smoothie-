0c4fdb452a3255b86efc4244c94de40507f1ed41
Meehan-007 <93944358+Meehan-007@users.noreply.github.com>
2024-07-11 18:45:13 -0400

    adding build for render

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ const app = express();
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
   app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
   app.use(cors({
     origin: 'https://apex-smoothie.onrender.com/'
